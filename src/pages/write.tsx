78082e974e2bcd83a936a8fcff284fa1fd2c9f4e
vjordan-cs <vjordan.cs@gmail.com>
2023-11-26 17:40:33 -0600

    update new entry page

--- src/pages/write.tsx
+++ src/pages/write.tsx
@@ -4,56 +4,68 @@ import { useRouter } from "next/router";
 import { useEffect, useState } from "react";
 import Loading from "@/components/Loading";
 import { api } from "@/utils/api";
-
+import BackButton from "@/components/BackButton";
+import EntryInput from "@/components/EntryInput";
 
 const write = () => {
-    const { status: sessionStatus } = useSession();
-    const { replace } = useRouter();
+  const { status: sessionStatus } = useSession();
+  const { replace } = useRouter();
 
-    const [journalEntry, setJournalEntry] = useState("");
+  const [journalEntry, setJournalEntry] = useState("");
 
-    useEffect(() => {
-        if (sessionStatus === "unauthenticated") {
-          replace("/");
-        }
-      }, [sessionStatus]);
-    
-      if (sessionStatus === "loading") {
-        return <Loading />;
-      }
+  useEffect(() => {
+    if (sessionStatus === "unauthenticated") {
+      replace("/");
+    }
+  }, [sessionStatus]);
 
+  if (sessionStatus === "loading") {
+    return <Loading />;
+  }
 
-    return(
-        <>
-            <Head>
-                <title>Write</title>
-            </Head>
-            <section className="mt-32 flex-col justify-center gap-10">
-            <h1 className="font-poppins text-center text-4xl font-bold text-neutral-50">
-            Write
-            </h1>
-            <form className="flex w-full flex-col justify-center gap-5">
-                <textarea
-                     cols={30}
-                     rows={10}
-                     className="font-montserrat mx-auto rounded-sm border border-slate-800 bg-gray-100 p-5 tracking-wide md:w-1/2"
-                     placeholder="How are you feeling?"
-                     value={journalEntry}
-                     onChange={(value) => setJournalEntry(value.target.value)}
-                     required
-                
-                
-                ></textarea>
-                <button
-                    type="submit"
-                    className="font-poppins mx-auto w-2/3 whitespace-pre-line rounded-sm bg-gradient-to-br from-gray-700 to-gray-800 py-3 text-xl font-bold text-gray-50 md:w-1/2"
-                  >
-                     Finish
-               </button>
-            </form>
-            </section>
-          </>
-        );
-    };
+  return (
+    <>
+      <Head>
+        <title>Write</title>
+      </Head>
+      <div className="relative h-screen bg-slate-400">
+        <div className="flex h-1/6 items-center justify-center px-14 text-center text-2xl">
+          <div>
+            <div>Journal Prompt</div>
+            <div>Lorem ipsum dolor sit amet consectetur adipisicing elit.</div>
+          </div>
+        </div>
+        <div className="flex h-5/6 justify-center">
+          <div className="h-5/6 w-5/6">
+            <EntryInput />
+          </div>
+        </div>
+        <BackButton />
+      </div>
+      {/* <section className="mt-32 flex-col justify-center gap-10">
+        <h1 className="font-poppins text-center text-4xl font-bold text-neutral-50">
+          Write
+        </h1>
+        <form className="flex w-full flex-col justify-center gap-5">
+          <textarea
+            cols={30}
+            rows={10}
+            className="font-montserrat mx-auto rounded-sm border border-slate-800 bg-gray-100 p-5 tracking-wide md:w-1/2"
+            placeholder="How are you feeling?"
+            value={journalEntry}
+            onChange={(value) => setJournalEntry(value.target.value)}
+            required
+          ></textarea>
+          <button
+            type="submit"
+            className="font-poppins mx-auto w-2/3 whitespace-pre-line rounded-sm bg-gradient-to-br from-gray-700 to-gray-800 py-3 text-xl font-bold text-gray-50 md:w-1/2"
+          >
+            Finish
+          </button>
+        </form>
+      </section> */}
+    </>
+  );
+};
 
-    export default write;
+export default write;
