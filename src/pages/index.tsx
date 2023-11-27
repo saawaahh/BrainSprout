c90d9abb4333f761cdc25cbdfaab59e757d18f83
vjordan-cs <vjordan.cs@gmail.com>
2023-11-27 15:25:29 -0600

    update landing

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -14,22 +14,27 @@ export default function Home() {
         <meta name="description" content="An AI journalling app" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
-      <div
-        className="flex h-screen items-center bg-lime-800"
-      >
-        <div className="m-auto mt-64 flex flex-col justify-center gap-5 text-center align-middle">
-          <h1 className="font-poppins bg-gradient-to-br from-white to-slate-600 box-decoration-slice bg-clip-text p-2 text-7xl font-extrabold text-transparent">
-            BrainSprout
-          </h1>
-          <h2 className="font-montserrat text-5xl text-neutral-100">
-            sprout your recovery
-          </h2>
-          <button
-            onClick={sessionData ? () => void signOut() : () => void signIn()}
-            className="font-poppins mx-auto rounded-sm bg-gradient-to-br from-indigo-500 to-indigo-600 px-20 py-2 text-2xl font-bold text-neutral-50 shadow-sm"
-          >
-            {sessionData ? "Sign Out" : "Sign In"}
-          </button>
+      <div className="h-screen md:flex">
+        <div className="flex h-2/3 items-center justify-center bg-slate-500 md:h-screen md:w-2/3">
+          <div className="text-center">
+            <div className="text-5xl md:text-6xl">☘️ BrainSprout</div>
+            <div className="p-10">
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Totam
+              exercitationem dolor consequuntur? Atque corrupti, illo magni
+              aliquid maxime animi omnis vero illum soluta dolore officiis
+              doloremque excepturi quae culpa velit!
+            </div>
+          </div>
+        </div>
+        <div className="flex h-1/3 items-center justify-center bg-red-200 md:h-screen md:w-1/3">
+          <div className="text-center">
+            <button
+              onClick={sessionData ? () => void signOut() : () => void signIn()}
+              className="rounded-lg border-2 border-slate-100 bg-slate-100 p-6"
+            >
+              {sessionData ? "Sign Out" : "Sign In"}
+            </button>
+          </div>
         </div>
       </div>
     </>
