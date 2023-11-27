1eebbc9a3881d4b71db507372a6a056203c31227
vjordan-cs <vjordan.cs@gmail.com>
2023-11-27 07:52:17 -0600

    update entries page

---
+++ src/components/EntryListItem.tsx
@@ -0,0 +1,8 @@
+export default function EntryListItem() {
+  return (
+    <div className="mx-4 mb-4 flex rounded-lg border-2 p-4 text-lg hover:bg-red-200 hover:bg-opacity-20">
+      <div className="w-1/3 md:w-1/4">November 30th, 2023</div>
+      <div className="w-2/3 border-l pl-6 md:w-3/4">Details here...</div>
+    </div>
+  );
+}
--- src/pages/entries/index.tsx
+++ src/pages/entries/index.tsx
@@ -6,46 +6,55 @@ import Head from "next/head";
 import { useRouter } from "next/router";
 import { useEffect } from "react";
 import Link from "next/link";
+import EntryListItem from "@/components/EntryListItem";
 
 const Entries = () => {
-    const { status: sessionStatus } = useSession(); 
-    const {replace} = useRouter();
+  const { status: sessionStatus } = useSession();
+  const { replace } = useRouter();
 
-    const { data: entriesData } = api.journalling.getAllEntries.useQuery(
-        undefined,
-        {
-          enabled: sessionStatus === "authenticated",
-        },
-      );
+  const { data: entriesData } = api.journalling.getAllEntries.useQuery(
+    undefined,
+    {
+      enabled: sessionStatus === "authenticated",
+    },
+  );
 
-    useEffect(() => {
-        if (sessionStatus === "unauthenticated") {
-            replace("/");
-        }
-    }, [sessionStatus]);
+  useEffect(() => {
+    if (sessionStatus === "unauthenticated") {
+      replace("/");
+    }
+  }, [sessionStatus]);
 
-    if (sessionStatus === "loading") {
-        return <Loading />;
-      }
-    
+  if (sessionStatus === "loading") {
+    return <Loading />;
+  }
 
-    return(
+  return (
     <>
-        <Head>
-            <title>Entries</title>
-
-        </Head>
-        <section className="mt-32 flex flex-col justify-center gap-10">
+      <Head>
+        <title>Entries</title>
+      </Head>
+      {/* <section className="mt-32 flex flex-col justify-center gap-10">
             <h1 className="text-center font-poppins text-4xl font-bold text-neutral-50">
                 Entries
             </h1>
             <NoEntries/>
            
-        </section>
+        </section> */}
+      <div className="relative h-screen bg-slate-400">
+        <div className="flex h-1/6 items-center justify-center px-14 text-center text-2xl">
+          <div>
+            <div>Entries</div>
+          </div>
+        </div>
+        <div className="h-5/6 overflow-auto">
+          {Array.from({ length: 5 }).map((_, index) => (
+            <EntryListItem key={index} />
+          ))}
+        </div>
+      </div>
     </>
- 
-    );
+  );
 };
-
 
 export default Entries;
